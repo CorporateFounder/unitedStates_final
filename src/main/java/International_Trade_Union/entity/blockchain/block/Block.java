bc6221dc236a90ea4a4ae6816c9263b003c0627d
ASUS <tuychievnetmat@gmail.com>
2023-06-03 13:35:20 +0500

    readme install

--- src/main/java/International_Trade_Union/entity/blockchain/block/Block.java
+++ src/main/java/International_Trade_Union/entity/blockchain/block/Block.java
@@ -146,13 +146,17 @@ public final class Block implements Cloneable {
         this.randomNumberProof = 0;
         String hash = "";
         while (true){
+
             this.randomNumberProof++;
+
+            System.out.println("UtilsBlock: Trying to find a block number: " + this.randomNumberProof);
             BlockForHash block = new BlockForHash(this.dtoTransactions,
                     this.previousHash, this.minerAddress, this.founderAddress,
                     this.randomNumberProof, this.minerRewards, this.hashCompexity, this.timestamp, this.index);
             hash = block.hashForTransaction();
             if(UtilsUse.hashComplexity(hash.substring(0, hashCoplexity), hashCoplexity))
             {
+                System.out.println("block found");
                 break;
             }
 
