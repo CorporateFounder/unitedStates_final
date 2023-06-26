35489fb7f5687387907105dc86e4c4fce75c5a31
ASUS <tuychievnetmat@gmail.com>
2023-06-26 20:18:48 +0500

    version 2 check validation

--- src/main/java/International_Trade_Union/entity/blockchain/Blockchain.java
+++ src/main/java/International_Trade_Union/entity/blockchain/Blockchain.java
@@ -420,6 +420,7 @@ public class Blockchain implements Cloneable{
                         System.out.println("ERROR: UtilsBlock: validation: block.Hash():" + block.getHashBlock());
                         System.out.println("ERROR: UtilsBlock: validation: BLOCK_GENERATION_INTERVAL:" + Seting.BLOCK_GENERATION_INTERVAL);
                         System.out.println("ERROR: UtilsBlock: validation: DIFFICULTY_ADJUSTMENT_INTERVAL:" + Seting.DIFFICULTY_ADJUSTMENT_INTERVAL);
+                        size++;
                         return new DataShortBlockchainInformation(size, valid, hashCount);
                     }
 
@@ -430,7 +431,7 @@ public class Blockchain implements Cloneable{
             }
         }
 
-
+        size++;
         return new DataShortBlockchainInformation(size, valid, hashCount);
     }
 
