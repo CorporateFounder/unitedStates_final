a68ec2736721b0f591c9eeecfb8c41fd63aa5274
ASUS <tuychievnetmat@gmail.com>
2023-07-02 23:10:35 +0500

    version 3 RESOLVE CONNECT

--- src/main/java/International_Trade_Union/controllers/MainController.java
+++ src/main/java/International_Trade_Union/controllers/MainController.java
@@ -3,6 +3,7 @@ package International_Trade_Union.controllers;
 import International_Trade_Union.about_us_engDraft.AboutUsEngDraft;
 import International_Trade_Union.config.BlockchainFactoryEnum;
 import International_Trade_Union.entity.blockchain.Blockchain;
+import International_Trade_Union.entity.blockchain.DataShortBlockchainInformation;
 import International_Trade_Union.governments.Director;
 import International_Trade_Union.governments.Directors;
 import International_Trade_Union.governments.UtilsGovernment;
@@ -107,6 +108,7 @@ public class MainController {
         model.addAttribute("size", size);
         model.addAttribute("version", Seting.VERSION);
         boolean validation = blockchain.validatedBlockchain();
+
         if(validation == false){
             System.out.println("deleted blockchain files");
 
