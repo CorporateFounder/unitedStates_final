de5879ef3e664ad0d446f3c60d59a6f273ec322e
ASUS <tuychievnetmat@gmail.com>
2023-07-04 19:15:11 +0500

    version 3 FINAL

--- src/main/java/International_Trade_Union/originalCorporateCharter/OriginalPreambleEng.java
+++ src/main/java/International_Trade_Union/originalCorporateCharter/OriginalPreambleEng.java
@@ -1,9 +1,7 @@
 package International_Trade_Union.originalCorporateCharter;
 
 public interface OriginalPreambleEng {
-    String ARTICLE_0 = " (By mistake, I added a draft to the bylaws, which has not yet been finalized, but not from scratch " +
-            "to mine blocks, I add here the original constitution-charter. This charter is signed by the founder and " +
-            "it is this statute that is valid. Grammar errors were also corrected)" +
+    String ARTICLE_0 =
             " International Trade Union Corporation is a cryptocurrency organization whose goal is to create " +
             " a unified trading eco-system that allows people from different parts of the world to sell, as well as acquire " +
             " high-quality goods and services, at the most affordable prices, while respecting human rights. " +
