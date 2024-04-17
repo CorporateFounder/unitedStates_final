51957bdc8014d6d3c2550b4841922ada80f00e2e
CorporateFounder <CorporateFounder@proton.me>
2024-04-17 12:45:47 +0500

    0.32.24 fixed sending block

--- src/main/java/International_Trade_Union/utils/UtilsStorage.java
+++ src/main/java/International_Trade_Union/utils/UtilsStorage.java
@@ -3,18 +3,24 @@ package International_Trade_Union.utils;
 import International_Trade_Union.setings.Seting;
 import org.json.JSONException;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.NoRouteToHostException;
 import java.net.SocketException;
 
 public class UtilsStorage {
-    public static int getSize(){
+    public static int getSize()  {
         String address = "http://194.87.236.238:82";
         for (String s : Seting.ORIGINAL_ADDRESSES) {
             address = s;
         }
+
         String sizeStr = "-1";
         try {
+            String server = UtilsFileSaveRead.read(Seting.YOUR_SERVER);
+            if (!server.isEmpty() && !server.isBlank()){
+                address = server;
+            }
             sizeStr = UtilUrl.readJsonFromUrl(address+ "/size");
         }catch (NoRouteToHostException e){
             System.out.println("home page you cannot connect to global server," +
