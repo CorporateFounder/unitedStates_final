afabfbe266c94fe6d722ac88cccb66cad18f5dd8
CorporateFounder <CorporateFounder@proton.me>
2024-08-26 11:21:52 +0500

    0.32.115 test

--- src/main/java/International_Trade_Union/setings/Seting.java
+++ src/main/java/International_Trade_Union/setings/Seting.java
@@ -348,7 +348,7 @@ public interface Seting {
 
     int ALGORITM_MINING = 295316;
 
-    int BALANCE_CHEKING = 296874;
+    int BALANCE_CHEKING = 297526;
     int ALGORITM_MINING_2 = 296583;
     int MULT = 6;
 
